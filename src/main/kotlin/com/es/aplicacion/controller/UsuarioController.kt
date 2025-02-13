65fdeedf208c1fd87922ac707e5598d4c8242ca6
rmungar1209 <rmungar1209@g.educaand.es>
2025-02-13 20:22:37 +0100

    cambio en los requests

--- src/main/kotlin/com/es/aplicacion/controller/UsuarioController.kt
+++ src/main/kotlin/com/es/aplicacion/controller/UsuarioController.kt
@@ -18,8 +18,9 @@ import org.springframework.security.core.Authentication
 import org.springframework.security.core.AuthenticationException
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-
+@RequestMapping("usuarios")
 @RestController
 class UsuarioController {
 
